eb0b2382f45cd59528b615952b67953d8ae61b5b
Pranaymeharkule <pranaymeharkule272005@gmail.com>
2025-03-18 11:17:03 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-from django.contrib.auth.forms import UserCreationForm
-
-
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Booking
-        fields = ('bookingnumber', 'status',)
